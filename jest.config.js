4db3e46f12d9ed45352e0fbeed91a53377990840
eddietindame <eddie.tindame@googlemail.com>
2021-03-31 14:33:41 +0100

    ts-jest pathsToModuleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,22 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig')
+
 module.exports = {
     roots: ['<rootDir>'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
     testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'babel-jest',
+        '^.+\\.(ts|tsx)$': 'babel-jest'
     },
     watchPlugins: [
         'jest-watch-typeahead/filename',
-        'jest-watch-typeahead/testname',
+        'jest-watch-typeahead/testname'
     ],
+    modulePaths: ['<rootDir>'],
     moduleNameMapper: {
+        ...pathsToModuleNameMapper(compilerOptions.paths),
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
     }
 }
