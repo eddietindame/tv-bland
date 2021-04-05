69e57796cb46de50231accd7ad0970223258030f
eddietindame <eddie.tindame@googlemail.com>
2021-04-05 20:58:03 +0100

    NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import App from 'next/app'
+import NProgress from 'next-nprogress/component'
 
 import '@/styles/index.scss'
 
 class MyApp extends App<any, any> {
     render() {
         const { Component, router, pageProps } = this.props
-        return <Component key={router.route} {...pageProps} />
+
+        return (
+            <>
+                <NProgress color="#000000" />
+                <Component key={router.route} {...pageProps} />
+            </>
+        )
     }
 }
 
