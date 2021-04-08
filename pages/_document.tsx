35b3006e6c49367fd39746b2913367b1160b7fd4
eddietindame <eddie.tindame@googlemail.com>
2021-04-09 00:08:33 +0100

    Html component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,15 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-
     render() {
         return (
-            <html lang="en-GB">
+            <Html lang="en-GB">
                 <Head />
                 <body>
                     <Main />
                     <NextScript />
                 </body>
-            </html>
+            </Html>
         )
     }
 }
